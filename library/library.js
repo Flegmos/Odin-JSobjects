b49d7abb2b639d0d60b6ca38139eb60ecaccf47a
Flegmos <frybortdaniel@gmail.com>
2026-08-18 11:26:11 +0200

    refresh instead of direct dom insert

--- library/library.js
+++ library/library.js
@@ -1,15 +1,20 @@
 document.addEventListener('DOMContentLoaded', initApp);
 
+const libraryContainer = document.getElementById('libraryContainer');
+let library = [];
+
 function initApp() {
     const dialog = document.getElementById("mydialog");
     const showButton = document.getElementById("showAddBook");
     const addButton = document.getElementById("addBook");
     const calcelButton = document.getElementById("cancelBook");
 
+
     const nameInput = document.getElementById("nameInputId");
     const genreInput = document.getElementById("genreInputId");
     const pageNumInput = document.getElementById("pageNumInputId");
     const descInput = document.getElementById("descInputId");
+
 
     // "Show the dialog" button opens the dialog modally
     showButton.addEventListener("click", () => {
@@ -47,41 +52,46 @@ function Book(name, genre, pageNum, desc) {
     this.genre = genre;
     this.pageNum = pageNum;
     this.desc = desc;
+    this.UUID = crypto.randomUUID();
 }
 
-let library = [];
 
 Book.prototype.addToPage = function () {
-    const wrapper = document.createElement('div');
-    wrapper.id = crypto.randomUUID();
-    wrapper.className = 'book-widget';
-    const namep = document.createElement('h3');
-    namep.innerText = this.name;
-    const pageNump = document.createElement('div');
-    pageNump.innerText = 'Pages: ' + this.pageNum
-    const genrep = document.createElement('div');
-    genrep.innerText = 'Genre: ' + this.genre
-    const descp = document.createElement('div');
-    descp.innerText = this.desc;
-    const deleteButton = document.createElement('button');
-    deleteButton.innerText = "X";
-    deleteButton.className = "delete-button";
-    deleteButton.onclick = onDeleteClick;
-    wrapper.append(namep, pageNump, genrep, descp, deleteButton);
-
-    const target = document.getElementById('libraryContainer');
-    target.appendChild(wrapper);
-
     library.push(this);
+    refreshBooks();
     console.log(library);
 };
 
-function onDeleteClick(){
+function refreshBooks() {
+    libraryContainer.innerHTML = '';
+    library.forEach ((book) => { 
+        const wrapper = document.createElement('div');
+        wrapper.id = book.UUID;
+        wrapper.className = 'book-widget';
+        const namep = document.createElement('h3');
+        namep.innerText = book.name;
+        const pageNump = document.createElement('div');
+        pageNump.innerText = 'Pages: ' + book.pageNum
+        const genrep = document.createElement('div');
+        genrep.innerText = 'Genre: ' + book.genre
+        const descp = document.createElement('div');
+        descp.innerText = book.desc;
+        const deleteButton = document.createElement('button');
+        deleteButton.innerText = "X";
+        deleteButton.className = "delete-button";
+        deleteButton.onclick = onDeleteClick;
+        wrapper.append(namep, pageNump, genrep, descp, deleteButton);
+        libraryContainer.appendChild(wrapper);
+
+    });
+};
+
+function onDeleteClick() {
     const deletedBookId = this.parentElement.id;
-    const deletedBookIndex =  library.findIndex((book) => {book.id == deletedBookId})
-    library.splice(deletedBookIndex,1);
+    const deletedBookIndex = library.findIndex((book) => { book.id == deletedBookId })
+    library.splice(deletedBookIndex, 1);
     console.log(library);
-    this.parentElement.remove();
+    refreshBooks();
 }
 
 
